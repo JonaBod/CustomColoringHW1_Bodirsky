18e2116c44a1860bfc52dbaaeacb15474955f09c
jonabodirsky <bodirsky24@up.edu>
2024-09-24 13:26:50 -0700

    git update

--- CS371/BirthdayCake-master/app/src/main/java/cs301/birthdaycake/CakeView.java
+++ CS371/BirthdayCake-master/app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -51,7 +51,7 @@ public class CakeView extends SurfaceView {
         setWillNotDraw(false);
 
         //Setup our palette
-        cakePaint.setColor(0xFFC755B5);  //violet-red
+        cakePaint.setColor(0xFFC755C5);  //violet-red
         cakePaint.setStyle(Paint.Style.FILL);
         frostingPaint.setColor(0xFFFFFACE);  //pale yellow
         frostingPaint.setStyle(Paint.Style.FILL);
